037f8726254d1d6c5917ab4b165aa1792fdbc0e2
swve <bdswve@gmail.com>
2022-04-10 15:19:29 +0200

    implement index search

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import { updateDate } from "@redux/actions";
 import InsideHeader from "@components/Header/InsideHeader";
-import { checkGitHubToken } from "@services/github";
 
 interface RootState {
   selectedDate: string;
@@ -27,8 +26,8 @@ export default function Home() {
     setSearchValue(parsedValues);
   };
 
-  async function testCheck() {
-    await checkGitHubToken();
+  const submitSearch = (e) => {
+    router.push("/calendar/github/" + searchValue[0] + "/" + searchValue[1]);
   }
 
   
@@ -36,7 +35,8 @@ export default function Home() {
   return (
     <HomePage>
       <InsideHeader></InsideHeader>
-      
+      <SearchBox onChange={handleSearchTextChange} className="" name="" id="" />{" "}
+      <button onClick={submitSearch} type="submit">Go</button>
     </HomePage>
   );
 }
@@ -44,4 +44,16 @@ export default function Home() {
 const HomePage = styled.div`
   background: linear-gradient(180deg, #09090a 0%, rgba(39, 49, 55, 0.52) 100%), linear-gradient(243.33deg, #4c15eb 5.62%, #245aaa 74.42%, #0dd1dd 127.92%);
   height: 100vh;
+`;
+
+const SearchBox = styled.input`
+  width: 235px;
+  height: 28px;
+  left: 681px;
+  top: 481px;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.37), rgba(0, 0, 0, 0.37));
+  border: 1px solid rgba(255, 255, 255, 0.09);
+  box-sizing: border-box;
+  border-radius: 10px;
+  color: aliceblue;
 `;
