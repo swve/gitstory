536607e364f34a843776fa9701e6b46327e5ed20
swve <bdswve@gmail.com>
2022-06-04 11:56:11 +0200

    add additional envs

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  env: {
+    // This is the default value, but we want to be explicit
+    GITHUB_SECRET: process.env.GITHUB_ID,
+    GITHUB_ID: process.env.GITHUB_SECRET,
+  },
 };
