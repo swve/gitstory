b7da64189e514271c1477907aad90182582968f2
swve <bdswve@gmail.com>
2023-04-04 22:09:14 +0200

    chore: remove plausible and use vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { store } from "@redux/store";
 import PlausibleProvider from "next-plausible";
+import { Analytics } from '@vercel/analytics/react';
 import { Session } from "next-auth";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{
@@ -12,14 +13,13 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{
 }>) {
   return (
     <>
-      <PlausibleProvider domain={process.env.ANALYTICS_DOMAIN} enabled customDomain={process.env.ANALYTICS_CUSTOM_DOMAIN} selfHosted={true}>
-        <SessionProvider session={session}>
-          <Provider store={store}>
-            <NextNProgress color="#f4f4f4" startPosition={0.3} stopDelayMs={1} height={4} options={{ easing: "ease", speed: 500, showSpinner: false }} />
-            <Component {...pageProps} />
-          </Provider>
-        </SessionProvider>
-      </PlausibleProvider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <NextNProgress color="#f4f4f4" startPosition={0.3} stopDelayMs={1} height={4} options={{ easing: "ease", speed: 500, showSpinner: false }} />
+          <Component {...pageProps} />
+        </Provider>
+      </SessionProvider>
+      <Analytics />
     </>
   );
 }
